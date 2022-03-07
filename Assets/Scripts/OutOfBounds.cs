e58729d9e8bbf724c6d5b9aa2d02cd4abc74e2e0
Spencer Lin <linspenc@usc.edu>
2022-03-06 22:35:20 -0800

    respawn script

---
+++ Assets/Scripts/OutOfBounds.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using UnityEngine;
+
+public class OutOfBounds : MonoBehaviour
+{
+    [SerializeField] private Animator canvasAnimator;
+    private static readonly int FadeOutBool = Animator.StringToHash("Fade Out");
+
+    // player
+    private GameObject _player;
+    private Vector3 _respawnPoint;
+    private FirstPersonMovement _playerMove;
+    private Jump _playerJump;
+    private Crouch _playerCrouch;
+
+    private void Start()
+    {
+        _player = GameObject.Find("Player");
+        _respawnPoint = _player.transform.position;
+        _playerMove = _player.GetComponent<FirstPersonMovement>();
+        _playerJump = _player.GetComponent<Jump>();
+        _playerCrouch = _player.GetComponent<Crouch>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == _player)
+        {
+            StartCoroutine(FadeOut());
+        }
+    }
+
+    private void TogglePlayerMovement(bool state)
+    {
+        _playerMove.enabled = state;
+        _playerJump.enabled = state;
+        _playerCrouch.enabled = state;
+    }
+
+    private IEnumerator FadeOut()
+    {
+        TogglePlayerMovement(false);
+        canvasAnimator.SetBool(FadeOutBool, true);
+        yield return new WaitForSeconds(0.5f);
+        canvasAnimator.SetBool(FadeOutBool, false);
+        _player.transform.position = _respawnPoint;
+        TogglePlayerMovement(true);
+    }
+}
